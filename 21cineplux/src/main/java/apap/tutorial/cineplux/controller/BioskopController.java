f52dd585fbf243b5d537dc85081292bc9f6c27ca
syndiyohana <71598534+syndiyohana@users.noreply.github.com>
2021-11-10 15:23:52 +0700

    Feat/tutorial 4 21cineplux (#10)
    * feat: add tutorial-4-21cineplux
    
    * docs: add notes to README.md
    
    * feat: fixing feature

--- 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BioskopController.java
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/controller/BioskopController.java
@@ -24,51 +24,82 @@ public class BioskopController {
     @Autowired
     private FilmService filmService;
 
-    //routing URL yang diinginkan
+    // routing URL yang diinginkan
     @GetMapping("/bioskop/add")
     public String addBioskopForm(Model model) {
-
-        //Add variabel id bioskop ke "idBioskop" untuk dirender ke dalam thymeleaf
-        model.addAttribute("idBioskop", new BioskopModel());
+        BioskopModel bioskop = new BioskopModel();
 
         List<FilmModel> listFilm = filmService.getListFilm();
+        List<FilmModel> listFilmNew = new ArrayList<FilmModel>();
+
+        bioskop.setListFilm(listFilmNew);
+        bioskop.getListFilm().add(new FilmModel());
+
+        // Add variabel id bioskop ke "idBioskop" untuk dirender ke dalam thymeleaf
+        model.addAttribute("bioskop", bioskop);
         model.addAttribute("listFilm", listFilm);
 
-        //return view template yang digunakan
+        // return view template yang digunakan
         return "form-add-bioskop";
     }
 
-    @PostMapping("/bioskop/add")
-    public String addBioskopSubmit(
-            @ModelAttribute BioskopModel bioskop,
-            Model model
-    ) {
+    @PostMapping(value = "/bioskop/add", params = {"save"})
+    public String addBioskopSubmit(@ModelAttribute BioskopModel bioskop, Model model) {
+        if (bioskop.getListFilm() == null) {
+            bioskop.setListFilm(new ArrayList<>());
+        }
         bioskopService.addBioskop(bioskop);
         model.addAttribute("noBioskop", bioskop.getNoBioskop());
         return "add-bioskop";
     }
 
+    @PostMapping(value = "/bioskop/add", params = {"addRow"})
+    private String addRowFilmMultiple(@ModelAttribute BioskopModel bioskop, Model model) {
+        if (bioskop.getListFilm() == null || bioskop.getListFilm().size() == 0) {
+            bioskop.setListFilm(new ArrayList<>());
+        }
+        bioskop.getListFilm().add(new FilmModel());
+        List<FilmModel> listFilm = filmService.getListFilm();
+
+        model.addAttribute("bioskop", bioskop);
+        model.addAttribute("listFilm", listFilm);
+
+        return "form-add-bioskop";
+    }
+
+    @PostMapping(value = "/bioskop/add", params = {"deleteRow"})
+    private String deleteRowFilmMultiple(@ModelAttribute BioskopModel bioskop, @RequestParam("deleteRow") Integer row,
+                                         Model model) {
+        final Integer rowId = Integer.valueOf(row);
+        bioskop.getListFilm().remove(rowId.intValue());
+
+        List<FilmModel> listFilm = filmService.getListFilm();
+
+        model.addAttribute("bioskop", bioskop);
+        model.addAttribute("listFilm", listFilm);
+
+        return "form-add-bioskop";
+    }
+
     @GetMapping("/bioskop/viewall")
     public String listBioskop(Model model) {
-        //Mendapatkan semua bioskop
+        // Mendapatkan semua bioskop
         List<BioskopModel> listBioskop = bioskopService.getBioskopList();
 
-        //Add variable semua BioskopModel ke 'listBioskop' untuk dirender dalam thymeleaf
+        // Add variable semua BioskopModel ke 'listBioskop' untuk dirender dalam
+        // thymeleaf
         model.addAttribute("listBioskop", listBioskop);
 
-        //Return view template yang diinginkan
+        // Return view template yang diinginkan
         return "viewall-bioskop";
     }
 
     @GetMapping("/bioskop/view")
-    public String viewDetailBioskop(
-            @RequestParam(value = "noBioskop") Long noBioskop,
-            Model model
-    ) {
-        //Mendapatkan bioskop sesuai dengan idBioskop
+    public String viewDetailBioskop(@RequestParam(value = "noBioskop") Long noBioskop, Model model) {
+        // Mendapatkan bioskop sesuai dengan idBioskop
         BioskopModel bioskop = bioskopService.getBioskopByNoBioskop(noBioskop);
         model.addAttribute("noBioskop", noBioskop);
-        if (bioskop == null){
+        if (bioskop == null) {
             return "error-page-id";
         }
 
@@ -83,13 +114,10 @@ public class BioskopController {
     }
 
     @GetMapping("/bioskop/update/{noBioskop}")
-    public String updateBioskopForm(
-            @PathVariable Long noBioskop,
-            Model model
-    ){
+    public String updateBioskopForm(@PathVariable Long noBioskop, Model model) {
         BioskopModel bioskop = bioskopService.getBioskopByNoBioskop(noBioskop);
         model.addAttribute("noBioskop", noBioskop);
-        if (bioskop == null){
+        if (bioskop == null) {
             return "error-page-id";
         }
 
@@ -98,40 +126,31 @@ public class BioskopController {
     }
 
     @PostMapping("/bioskop/update")
-    public String updateBioskopSubmit(
-            @ModelAttribute BioskopModel bioskop,
-            Model model
-    ){
+    public String updateBioskopSubmit(@ModelAttribute BioskopModel bioskop, Model model) {
         bioskopService.updateBioskop(bioskop);
         model.addAttribute("noBioskop", bioskop.getNoBioskop());
         return "update-bioskop";
     }
 
     @GetMapping("/bioskop/delete/{noBioskop}")
-    public String deleteBioskop(
-            @PathVariable Long noBioskop,
-            Model model
-    ){
+    public String deleteBioskop(@PathVariable Long noBioskop, Model model) {
         BioskopModel bioskop = bioskopService.getBioskopByNoBioskop(noBioskop);
 
         model.addAttribute("bioskop", bioskop);
         model.addAttribute("waktuTutup", bioskop.getWaktuTutup());
         model.addAttribute("noBioskop", bioskop.getNoBioskop());
 
-        if (bioskop.getListPenjaga().isEmpty()){
-            if (LocalTime.now().isAfter(bioskop.getWaktuTutup()) || LocalTime.now().isBefore(bioskop.getWaktuBuka())){
+        if (bioskop.getListPenjaga().isEmpty()) {
+            if (LocalTime.now().isAfter(bioskop.getWaktuTutup()) || LocalTime.now().isBefore(bioskop.getWaktuBuka())) {
                 bioskopService.deleteBioskop(bioskop);
                 return "delete-bioskop";
-            }
-            else{
+            } else {
                 return "error-delete-waktu";
             }
-        }
-        else {
-            if (LocalTime.now().isAfter(bioskop.getWaktuTutup()) || LocalTime.now().isBefore(bioskop.getWaktuBuka())){
+        } else {
+            if (LocalTime.now().isAfter(bioskop.getWaktuTutup()) || LocalTime.now().isBefore(bioskop.getWaktuBuka())) {
                 return "error-delete-bioskop1";
-            }
-            else{
+            } else {
                 return "error-delete-bioskop2";
             }
         }
